ecdc86999a7eb30223209887801ec8bbeac1cc9e
Benjamin Barenblat <bbarenblat@galois.com>
2014-01-23 16:56:28 -0800

    Correct license block in src/timing.c
    The license block in src/timing.c suggested that it was not
    GPL-licensed.  That is, however, incorrect; since the release of
    ppamltracer, src/timing.c has been licensed under the GPLv3+.  The
    license block is now corrected.

--- src/timing.c
+++ src/timing.c
@@ -1,5 +1,22 @@
 /* timing.c -- cross-platform wall time
- * Copyright (C) 2013  Galois, Inc.  All rights reserved. */
+ * Copyright (C) 2013, 2014  Galois, Inc.
+ *
+ * This library is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * To contact Galois, complete the Web form at
+ * <http://corp.galois.com/contact/> or write to Galois, Inc., 421 Southwest
+ * 6th Avenue, Suite 300, Portland, Oregon, 97204-1622. */
 
 #ifdef HAVE_CONFIG_H
 #	include <config.h>
